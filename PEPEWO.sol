588e68161d1a94575a489a23a7a2cfe132366034
pepewojakio <132935693+pepewojakio@users.noreply.github.com>
2023-05-14 02:44:33 +0300

    Create PEPEWO.sol

---
+++ PEPEWO.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+contract PEPEWO is ERC20, Ownable {
+    using Address for address payable;
+
+    IUniswapV2Router02 public uniswapV2Router;
+    address public  uniswapV2Pair;
+
+    mapping (address => bool) private _isExcludedFromFees;
+
+    string  public creator;
+
+    uint256 public  marketingFeeOnBuy;
+    uint256 public  marketingFeeOnSell;
+
+    address public  nftBank;
+
+    uint256 public  swapTokensAtAmount;
+    bool    private swapping;
+
+    bool    public swapEnabled;
+
+    event ExcludeFromFees(address indexed account, bool isExcluded);
+    event SwapAndSendMarketing(uint256 tokensSwapped, uint256 bnbSend);
+    event SwapTokensAtAmountUpdated(uint256 swapTokensAtAmount);
+
+    constructor () ERC20("PEPEWOJAK", "PEPEWO") 
+    {   
+        address router;
+        if (block.chainid == 56) {
+            router = 0x10ED43C718714eb63d5aA57B78B54704E256024E; // BSC Pancake Mainnet Router
+        } else if (block.chainid == 97) {
+            router = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1; // BSC Pancake Testnet Router
+        } else if (block.chainid == 1 || block.chainid == 5) {
+            router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; // ETH Uniswap Mainnet % Testnet
+        } else {
+            revert();
+        }
+
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(router);
+        address _uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
+            .createPair(address(this), _uniswapV2Router.WETH());
+
+        uniswapV2Router = _uniswapV2Router;
+        uniswapV2Pair   = _uniswapV2Pair;
+
+        _approve(address(this), address(uniswapV2Router), type(uint256).max);
+
+        marketingFeeOnBuy  = 0;
+        marketingFeeOnSell = 5;
+
+        nftBank = 0x94cC8fD978EFa6BeF13F40fab2379fcbA40BC5A2;
+
+        _isExcludedFromFees[owner()] = true;
+        _isExcludedFromFees[address(0xdead)] = true;
+        _isExcludedFromFees[address(this)] = true;
+        _isExcludedFromFees[0x407993575c91ce7643a4d4cCACc9A98c36eE1BBE] = true; //pinklock
+
+        _mint(owner(), 1e11 * (10 ** decimals()));
+        swapTokensAtAmount = totalSupply() / 5_000;
+
+        tradingEnabled = false;
+        swapEnabled = false;
+    }
+
+    receive() external payable {
+
+  	}
+
+    function claimStuckTokens(address token) external onlyOwner {
+        require(token != address(this), "Owner cannot claim contract's balance of its own tokens");
+        if (token == address(0x0)) {
+            payable(msg.sender).sendValue(address(this).balance);
+            return;
+        }
+        IERC20 ERC20token = IERC20(token);
+        uint256 balance = ERC20token.balanceOf(address(this));
+        ERC20token.transfer(msg.sender, balance);
+    }
+
+    function excludeFromFees(address account, bool excluded) external onlyOwner{
+        require(_isExcludedFromFees[account] != excluded,"Account is already the value of 'excluded'");
+        _isExcludedFromFees[account] = excluded;
+
+        emit ExcludeFromFees(account, excluded);
+    }
+
+    function isExcludedFromFees(address account) public view returns(bool) {
+        return _isExcludedFromFees[account];
+    }
+
+    bool public tradingEnabled;
+
+    function enableTrading() external onlyOwner{
+        require(!tradingEnabled, "Trading already enabled.");
+        tradingEnabled = true;
+        swapEnabled = true;
+    }
+
+    function _transfer(address from,address to,uint256 amount) internal  override {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(tradingEnabled || _isExcludedFromFees[from] || _isExcludedFromFees[to], "Trading not yet enabled!");
+       
+        if (amount == 0) {
+            super._transfer(from, to, 0);
+            return;
+        }
+
+		uint256 contractTokenBalance = balanceOf(address(this));
+
+        bool canSwap = contractTokenBalance >= swapTokensAtAmount;
+
+        if (canSwap &&
+            !swapping &&
+            to == uniswapV2Pair &&
+            swapEnabled
+        ) {
+            swapping = true;
+
+            swapAndSendMarketing(contractTokenBalance);     
+
+            swapping = false;
+        }
+
+        uint256 _totalFees;
+        if (_isExcludedFromFees[from] || _isExcludedFromFees[to] || swapping) {
+            _totalFees = 0;
+        } else if (from == uniswapV2Pair) {
+            _totalFees = marketingFeeOnBuy;
+        } else if (to == uniswapV2Pair) {
+            _totalFees =  marketingFeeOnSell;
+        } else {
+            _totalFees = 0;
+        }
+
+        if (_totalFees > 0) {
+            uint256 fees = (amount * _totalFees) / 100;
+            amount = amount - fees;
+            super._transfer(from, address(this), fees);
+        }
+
+
+        super._transfer(from, to, amount);
+    }
+
+    function setSwapEnabled(bool _enabled) external onlyOwner{
+        require(swapEnabled != _enabled, "swapEnabled already at this state.");
+        swapEnabled = _enabled;
+    }
+
+    function setSwapTokensAtAmount(uint256 newAmount) external onlyOwner{
+        require(newAmount > totalSupply() / 1_000_000, "SwapTokensAtAmount must be greater than 0.0001% of total supply");
+        swapTokensAtAmount = newAmount;
+
+        emit SwapTokensAtAmountUpdated(swapTokensAtAmount);
+    }
+
+    function swapAndSendMarketing(uint256 tokenAmount) private {
+        uint256 initialBalance = address(this).balance;
+
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            tokenAmount,
+            0,
+            path,
+            address(this),
+            block.timestamp);
+
+        uint256 newBalance = address(this).balance - initialBalance;
+
+        payable(nftBank).sendValue(newBalance);
+
+        emit SwapAndSendMarketing(tokenAmount, newBalance);
+    }
+}
